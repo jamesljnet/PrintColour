a3b06063763eaab2db7b9db0f7a1befa8dbcfd9e
James Smith <james@lesjackson.net>
2022-10-10 19:43:57 +1100

    add ability to print green

--- Program.cs
+++ Program.cs
@@ -2,3 +2,8 @@
 Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine("--> Print Red");
 Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("--> Print Green");
+Console.ResetColor();
+
